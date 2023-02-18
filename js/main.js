43c7deefcfbaab3c37a813c7fc31104227cd8424
tezalve <tezalve@gmail.com>
2023-02-18 08:34:45 +0600

    :sparkles: added javascript for all other shapes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// Set up the first 3 functions so that all I need is the clicked buttons id and the area formula
+
 // geting innerhtml to get the shape name
-function getName(string){
-    return document.getElementById(string).parentElement.parentElement.parentElement.children[0].innerHTML;
+function getShape(string){
+    return document.getElementById(string).parentElement.children[0].innerHTML;
 }
 
 // adding html element using javascript
@@ -14,14 +16,59 @@ function multi(num1, num2){
     let num2Value = num2.value;
     num1Value = parseFloat(num1Value);
     num2Value = parseFloat(num2Value);
-    return (num1Value*num2Value).toFixed(2);
+    return (num1Value*num2Value).toFixed(1);
 }
 
 // when calculate btn is clicked for triangle
-document.getElementById('tribtn').addEventListener('click', function(){
-    const triangleB = document.getElementById('trib');
-    const triangleH = document.getElementById('trih');
+document.getElementById('tribtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
     let TriArea = multi(triangleB, triangleH);
-    showResult(TriArea, getName('trib'));
-    console.log(TriArea);
+    showResult(TriArea, getShape(id));
+});
+
+// when calculate btn is clicked for rectangle
+document.getElementById('rectbtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
+    let TriArea = multi(triangleB, triangleH);
+    showResult(TriArea, getShape(id));
+});
+
+// when calculate btn is clicked for parallelogram
+document.getElementById('parabtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
+    let TriArea = multi(triangleB, triangleH);
+    showResult(TriArea, getShape(id));
+});
+
+// when calculate btn is clicked for rhombus
+document.getElementById('rhombtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
+    let TriArea = 0.5*multi(triangleB, triangleH);
+    showResult(TriArea, getShape(id));
+});
+
+// when calculate btn is clicked for pentagon
+document.getElementById('pentbtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
+    let TriArea = 0.5*multi(triangleB, triangleH);
+    showResult(TriArea, getShape(id));
+});
+
+// when calculate btn is clicked for Ellipse
+document.getElementById('ellibtn').addEventListener('click', function(e){
+    const id = e.target.id;
+    const triangleB = document.getElementById(id).parentElement.children[2].children[0].children[0];
+    const triangleH = document.getElementById(id).parentElement.children[2].children[2].children[0];
+    let TriArea = 3.1416*multi(triangleB, triangleH);
+    showResult(TriArea, getShape(id));
 });
